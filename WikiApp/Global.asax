e2f8f6e0060888aa131e22bdd59537d6ccc92e50
Shounin <gstyrmir@gmail.com>
2014-05-05 11:00:49 +0000

    Initial Commit

---
+++ WikiApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WikiApp.MvcApplication" Language="C#" %>
---
+++ WikiApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WikiApp.Startup))]
+namespace WikiApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
