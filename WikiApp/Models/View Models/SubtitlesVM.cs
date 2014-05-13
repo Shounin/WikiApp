e254b0573337c9dc12237cb3d32695aee30298c7
Marteinn Guðberg Þorláksson <marsi2103@gmail.com>
2014-05-13 16:57:13 +0000

    Added file to subtitle file

--- WikiApp/Models/View Models/SubtitlesVM.cs
+++ WikiApp/Models/View Models/SubtitlesVM.cs
@@ -10,10 +10,8 @@ namespace WikiApp.Models.View_Models
         
         public IEnumerable<SubtitleFile> allMovies { get; set; }
         public IEnumerable<SubtitleFile> allTV { get; set; }
-
         public IEnumerable<SubtitleFile> allFiles { get; set; }
         public char FirstLetter { get; set; }
-
         public SubtitleFile subtitleFile { get; set; }
     }
 }
