d916f258a087f86362e339a9adbc82680d6a0366
Shounin <gstyrmir@gmail.com>
2014-05-14 16:51:48 +0000

    Added Upvote model

---
+++ WikiApp/Models/Upvote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WikiApp.Models
+{
+	public class Upvote
+	{
+		public int ID { get; set; }
+
+		public int SubtitleID { get; set; }
+		
+		public int userID { get; set; }
+	}
+}
